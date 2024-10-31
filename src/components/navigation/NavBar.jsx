9e71ab2a2ab612f01715441ece5aa059fce4932e
madaandreea <mada10050@gmail.com>
2024-10-31 21:14:47 +0200

    fixed more routings

--- src/components/navigation/NavBar.jsx
+++ src/components/navigation/NavBar.jsx
@@ -2,35 +2,73 @@ import SearchBar from "../search-bar/SearchBar";
 import LogInBtn from "../logging-btn/LogInBtn";
 import "./NavBar.css";
 
-function NavBar() {
-  return (
-    <nav className="nav-container">
-      <div className="pages-container">
-        <a href="" className="Logo-Img">
-          <img
-            src="https://i.pinimg.com/736x/62/e2/e1/62e2e1fd6e131aa7a97456e471deda19.jpg"
-            alt="RecipeLogo"
-            className="LogoImg"
-          />
-        </a>
-        <ul>
-          <li>
-            <a href="">Home</a>
-          </li>
-          <li>
-            <a href="/Recipes">Recipes</a>
-          </li>
-          <li>
-            <a href="/MyRecipes">My Recipes</a>
-          </li>
-        </ul>
-      </div>
-      <div className="wrapper">
-        <SearchBar></SearchBar>
-        <LogInBtn></LogInBtn>
-      </div>
-    </nav>
-  );
-}
 
+// function NavBar() {
+  
+//   return (
+//     <nav className="nav-container">
+//       <div className="pages-container">
+//         <a href="" className="Logo-Img">
+//           <img
+//             src="https://i.pinimg.com/736x/62/e2/e1/62e2e1fd6e131aa7a97456e471deda19.jpg"
+//             alt="RecipeLogo"
+//             className="LogoImg"
+//           />
+//         </a>
+//         <ul>
+//           <li>
+//             <a href="">Home</a>
+//           </li>
+//           <li>
+//             <a href="/Recipes">Recipes</a>
+//           </li>
+//           <li>
+//             <a href="/MyRecipes">My Recipes</a>
+//           </li>
+//         </ul>
+//       </div>
+//       <div className="wrapper">
+//         <SearchBar></SearchBar>
+//         <LogInBtn></LogInBtn>
+//       </div>
+//     </nav>
+//   );
+// }
+
+// export default NavBar;
+
+
+
+import { Link } from "react-router-dom";
+
+function NavBar() {
+  return(
+<nav className="nav-container">
+  <div className="pages-container">
+    <Link to="/" className="Logo-Img">
+      <img
+        src="https://i.pinimg.com/736x/62/e2/e1/62e2e1fd6e131aa7a97456e471deda19.jpg"
+        alt="RecipeLogo"
+        className="LogoImg"
+      />
+    </Link>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/Recipes">Recipes</Link>
+      </li>
+      <li>
+        <Link to="/MyRecipes">My Recipes</Link>
+      </li>
+    </ul>
+  </div>
+  <div className="wrapper">
+    <SearchBar />
+    <LogInBtn />
+  </div>
+</nav>
+  )
+}
 export default NavBar;
--- src/main.jsx
+++ src/main.jsx
@@ -65,3 +65,42 @@ createRoot(document.getElementById("root")).render(
 );
 
 
+
+
+
+
+// Import necessary modules from react and react-router-dom
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// import { HashRouter as Router, Route, Routes } from "react-router-dom";
+// import "./index.css";
+
+// // Import pages
+// import HomePage from "./pages/home-page/home-page";
+// import LogInPage from "./pages/login-page/login-page";
+// import RegisterPage from "./pages/register-page/register-page";
+// import MyRecipesPage from "./pages/my-recipes-page/my-recipes-page";
+// import AddRecipe from "./pages/add-recipe-page/add-recipe-page";
+// import UsersRecipePage from "./pages/user-recipe-page/users-recipes";
+// import MyRecipeDetails from "./pages/my-recipes-details/my-recipes-details";
+// import UserRecipeDetails from "./pages/users-recipes-details/users-recipes-details";
+// import EditRecipePage from "./pages/edit-page/edit-page";
+
+// // Configure routes using HashRouter
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <Router basename="/MyRecipeApp">
+//       <Routes>
+//         <Route path="/" element={<HomePage />} />
+//         <Route path="/LOGIN" element={<LogInPage />} />
+//         <Route path="/REGISTER" element={<RegisterPage />} />
+//         <Route path="/Recipes" element={<UsersRecipePage />} />
+//         <Route path="/MyRecipes" element={<MyRecipesPage />} />
+//         <Route path="/Add-Recipe" element={<AddRecipe />} />
+//         <Route path="/my-recipe/:recipeTitle" element={<MyRecipeDetails />} />
+//         <Route path="/other-recipe/:recipeTitle" element={<UserRecipeDetails />} />
+//         <Route path="/edit-recipe/:recipeId" element={<EditRecipePage />} />
+//       </Routes>
+//     </Router>
+//   </StrictMode>
+// );
